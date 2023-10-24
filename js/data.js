a4ab9f33c8a27afc61e780d87259674fde8dd50f
aSwanting <gab.damico@gmail.com>
2023-10-24 15:08:17 +0200

    add: alt text to slides object, vue element

--- js/data.js
+++ js/data.js
@@ -1,22 +1,27 @@
 const slides = [
     {
         image: './img/01.webp',
+        alt: 'Spiderman Miles Morales',
         title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     }, {
         image: './img/02.webp',
+        alt: 'Ratchet & Clank: Rift Apart',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
     }, {
         image: './img/03.webp',
+        alt: 'Fortnite',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
     }, {
         image: './img/04.webp',
+        alt: 'Stray',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
     }, {
         image: './img/05.webp',
+        alt: 'Avengers',
         title: "Marvel's Avengers",
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
