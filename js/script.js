87c97e27220c166485eaf0d49f897b02cc793419
aSwanting <gab.damico@gmail.com>
2023-10-24 15:13:36 +0200

    add: active classe follows currentIndex

--- js/script.js
+++ js/script.js
@@ -16,11 +16,12 @@ createApp({
 
     // Methods
     methods: {
-        changeImage(clicked) {
+        changeImage(clicked, thumbIndex) {
 
             // Perdonami Donato
             if (clicked === 'prev') { this.currentIitem > 0 ? this.currentIitem-- : this.currentIitem = this.carouselItems.length - 1 }
             if (clicked === 'next') { this.currentIitem < this.carouselItems.length - 1 ? this.currentIitem++ : this.currentIitem = 0 }
+            if (clicked === 'thumb') { this.currentIitem  = thumbIndex }
 
         }
     },
